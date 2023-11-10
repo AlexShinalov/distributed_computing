da3d0ea7cba88e2f5a6d097909f604b912234364
AlexShinalov <snw0987@gmail.com>
2023-11-10 19:27:24 +0300

    workable version

--- main.py
+++ main.py
@@ -1,4 +1,59 @@
-from tasks import add_numbers
+import requests
+from flask import send_from_directory
+from flask import Flask, request, render_template, send_file
+from tasks import apply_sharpen_filter
+from flask import Flask, request, render_template
+from io import BytesIO
+import os
 
-result = add_numbers.apply_async(args=[1, 5])
-result.get()
+app = Flask(__name__)
+
+# Настройка Celery
+app.config.update(
+    CELERY_BROKER_URL='pyamqp://guest:guest@192.168.2.218/',
+    CELERY_RESULT_BACKEND='rpc://guest:guest@192.168.2.218//'
+
+)
+
+# Инициализация Celery
+from celery import Celery
+
+celery = Celery('tasks', broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+
+@app.route('/') #главная страница
+def index():
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>') #страница загрузки для передачи на ноду
+def uploaded_file(filename):
+    return send_from_directory('uploads', filename)
+
+
+@app.route('/process_image', methods=['POST']) #страница обработки
+def process_image():
+    if 'image' not in request.files:
+        return "Изображение не загружено", 400
+
+    image_file = request.files['image']
+
+    if image_file and image_file.filename.endswith(('jpg', 'jpeg', 'png')):
+        image_path = os.path.join('uploads', image_file.filename)
+        image_file.save(image_path)
+
+        base_url = request.url_root  # Базовый URL текущего приложения
+        uploaded_image_url = f'{base_url}{image_path}'
+
+        result = apply_sharpen_filter.apply_async(args=[image_path])
+        processed_image_path = result.get()
+
+        response = requests.get(processed_image_path)
+
+        return send_file(BytesIO(response.content), mimetype='image/png')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
--- tasks.py
+++ tasks.py
@@ -1,10 +1,27 @@
+import requests
 from celery import Celery
-from PIL import Image
+import cv2
+import numpy as np
 import os
 
-app = Celery('tasks', broker='pyamqp://guest:guest@192.168.2.84//')
+app = Celery('tasks', broker='pyamqp://guest:guest@192.168.2.218/',
+             backend='rpc://guest:guest@192.168.2.218//')
 
-@app.task
-def add_numbers(x, y):
-    result = x + y
-    return result
+
+@app.task()
+def apply_sharpen_filter(image_url):
+    base_url = 'http://192.168.2.218'
+    image_url = f'{base_url}/{image_url}'
+    response = requests.get(image_url)
+    image = cv2.imdecode(np.frombuffer(response.content, np.uint8), -1)
+
+    # kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    # sharpened = cv2.filter2D(image, -1, kernel)
+    sharpened = cv2.resize(image, (40, 200))
+    output_path = "upload"
+    output_file = os.path.join(output_path, "done.jpg")
+    cv2.imwrite(output_file, sharpened)
+    # uploaded_file(os.path.basename(image_url))
+    path = 'http://192.168.2.128:5001/upload/done.jpg'
+
+    return path
