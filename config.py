b92c65f997ef57d1cc4d58daf7a66eccea22ac03
AlexShinalov <68298204+AlexShinalov@users.noreply.github.com>
2023-12-08 19:13:31 +0300

    Update config.py
    added broker & noda

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ from celery import Celery
 app = Celery('tasks', broker='pyamqp:///guest:guest@192.168.2.84//')
 
 app.config_from_object('celery_config')
+
+broker = '192.168.2.84'
+noda = '192.168.2.128'
